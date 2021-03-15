4a57ee5094563271612359608e910f5dae938e13
haraguchi12 <masakazu09201201@gmail.com>
2021-03-15 15:04:06 +0000

    編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,37 @@
 class BooksController < ApplicationController
-  def books
+  def new
+    @book = Book.new
+  end
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 詳細画面へリダイレクト
+    redirect_to book_path(book.id)
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
