e5397c6906a5e50c8473e03fa949976b4609438e
haraguchi12 <masakazu09201201@gmail.com>
2021-03-18 07:00:05 +0000

    テスト前完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,28 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
-  end
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @book = Book.new  
+  end  
   
   
   def create
     # １. データを新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+    if @book.save
+      flash[:notice] = "successfully"
+      # 詳細画面へリダイレクト
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:notice] = "error"
+      render :index
+    end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
@@ -26,15 +34,25 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    
+    if @book.update(book_params)
+    flash[:notice] = "successfully" 
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    flash.now[:notice] = "error"
+    render :edit
+    
+    end
+  
   end
   
   def destroy
      book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     redirect_to books_path  # 投稿一覧画面へリダイレクト
+    flash[:notice] = "successfully"
   end
 
   private
